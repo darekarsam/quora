c080e781f7db288c97f602c3b147d5274be7baf7
sameer <sameer.v.darekar@gmail.com>
2017-04-22 18:19:26 -0400

    intermediarry commit

--- vector.py
+++ vector.py
@@ -2,6 +2,8 @@ import pandas as pd
 from gensim import models
 import re
 import numpy as np
+from nltk.corpus import stopwords
+from nltk.stem import *
 
 print "Reading Dataframe..."
 trainDF = pd.read_csv("Data/train.csv")
@@ -10,20 +12,36 @@ testDF = pd.read_csv("Data/test.csv")
 trainDF.question1.fillna('abc', inplace=True)
 trainDF.question2.fillna('abc', inplace=True)
 
-train = trainDF.sample(frac=0.8,random_state=200)
-test = trainDF.drop(train.index)
-
-
 def cleanQuestion(question):
     """Functions to clean question pairs
     """
     # convert to lower case
     question = question.lower()
+#     question = question.replace()
+    question = question.decode('utf-8')
+    stop = stopwords.words('english')
+    stemmer = PorterStemmer()
+    import ipdb; ipdb.set_trace()
+    questions = re.sub(r'[^\x00-\x7F]+', ' ', question, flags=re.MULTILINE)
+    question = ' '.join([word for word in question.split() if word not in stop])
+    question = ' '.join([stemmer.stem(word) for word in question.split()])
     # remove extra spaces
     question = re.sub(r'[\s]+', ' ', question, flags=re.MULTILINE)
     # remove all punctuations
     question = re.sub(r'[^a-zA-Z]', ' ', question, flags=re.MULTILINE)
     return question
+
+def getLabeledSentence(questions, label):
+    labeledQuestions=[]
+    for uid, line in enumerate(questions):
+        labeledQuestions.append( models.doc2vec.LabeledSentence(words=line.split(), tags=[label+str(uid)]))
+    
+    return labeledQuestions
+    
+trainDF.question1 = trainDF.question1.apply(cleanQuestion)
+trainDF.question2 = trainDF.question2.apply(cleanQuestion)
+
+
 
 def getLabeledSentence(questions, label):
     labeledQuestions=[]
@@ -40,9 +58,8 @@ print "Cleaning Dataframe..."
 train.question1 = train.question1.apply(cleanQuestion)
 train.question2 = train.question2.apply(cleanQuestion)
 
-
-labeledQuestions1 = getLabeledSentence(train.question1.tolist(), 'question1')
-labeledQuestions2 = getLabeledSentence(train.question2.tolist(), 'question2')
+labeledQuestions1 = getLabeledSentence(trainDF.question1.tolist(), 'question1')
+labeledQuestions2 = getLabeledSentence(trainDF.question2.tolist(), 'question2')
 model = models.Doc2Vec(alpha=.025, min_alpha=.025, min_count=1)
 labeledQuestions = labeledQuestions1 + labeledQuestions2
 
@@ -53,7 +70,7 @@ print "Training model..."
 model.train(labeledQuestions)
 
 print "Saving model..."
-model.save('Models/4_8model')
+model.save('Models/4_23model')
 
 model_loaded = Doc2Vec.load('Models/4_8model')
 
